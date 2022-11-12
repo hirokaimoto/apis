c05ca6233a48e24486048b0ff1270f20e71376fc
Yoschihiro Kaimoto <116176473+hirokaimoto@users.noreply.github.com>
2022-11-12 12:10:52 -0300

    project conclusion - added weather api and styling

--- ex004/index.js
+++ ex004/index.js
@@ -1,9 +1,10 @@
 const author = document.getElementById('author-name')
 const cryptoHeader = document.getElementById('crypto-header')
 const cryptoData = document.getElementById('crypto-data')
+const timeEl = document.getElementById('time')
+const weather = document.getElementById('weather')
 
-
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+    fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then(res => res.json())
     .then(data => {
         console.log(data.urls.regular)
@@ -39,4 +40,65 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     })
     .catch(err => console.error(err))
 
-console.log(cryptoData.innerHTML)
+
+
+function displayTime() {
+const timeNow = new Date().toLocaleTimeString ('en-US', 
+        { hour12: true, 
+        hour: "numeric", 
+        minute: "numeric"});
+
+timeEl.innerHTML = timeNow
+
+}
+
+setInterval(displayTime, 1000)
+
+
+navigator.geolocation.getCurrentPosition(position => {
+    console.log(position.coords.latitude)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if(!res.ok) {
+                throw Error('Weather data not available')
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data.main.temp)
+            console.log(data.weather[0].icon)
+            weather.innerHTML = `
+            <img src='http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'>
+            <p class='temp'>${Math.round(data.main.temp)}°</p>
+            <p class='city'>${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+})
+
+// function showLocation(position) {
+//     var latitude = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//     alert("Latitude : " + latitude + " Longitude: " + longitude);
+//  }
+
+//  function errorHandler(err) {
+//     if(err.code == 1) {
+//        alert("Error: Access is denied!");
+//     } else if( err.code == 2) {
+//        alert("Error: Position is unavailable!");
+//     }
+//  }
+    
+//  function getLocation() {
+
+//     if(navigator.geolocation) {
+       
+//        // timeout at 60000 milliseconds (60 seconds)
+//        var options = {timeout:60000};
+//        navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+//     } else {
+//        alert("Sorry, browser does not support geolocation!");
+//     }
+//  }
+//  getLocation()
